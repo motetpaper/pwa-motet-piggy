d548f7f7b4c1b50c9ad2c381aa71113bac7c8bcb
motetpaper <71auras_aegis@icloud.com>
2025-01-31 11:02:19 -0800

    Update app.js - adds CEDICT vocablist below pinyin frame
    * uses JSON files generated using CEDICT text file
    * segments vocabulary with Intl.Segmenter
    * displays results below the main pinyin frame

--- app.js
+++ app.js
@@ -5,6 +5,9 @@
 // lic    : MIT
 //
 
+import vocab1 from './two.json' with {type: 'json'}
+import vocab2 from './one.json' with {type: 'json'}
+
 import { PiggyObject } from './obj/PiggyObject.js';
 
 const inputs = document.querySelector('#inputs');
@@ -43,6 +46,37 @@ function go() {
   const piggy = new PiggyObject();
   piggy.setInput(str).build();
   outputs.innerHTML = piggy.asHTML();
+  vocabs.innerHTML = addVocab();
+}
+
+
+function addVocab() {
+  const s = inputs.value;
+  const options = { granularity: 'word' };
+  const segm = new Intl.Segmenter('zh', options);
+  const segments = segm.segment(s);
+  const map = Array.from(segments);
+  console.log('two-words-vocab');
+  const words = map
+    .filter((a)=>a.segment.length<3)
+    .filter((a)=>a.segment.match(/\W/))
+    .filter((a)=>!!a.isWordLike)
+    .map((a)=>a.segment);
+
+  const vocab = [].concat(vocab1,vocab2);
+
+// use this to add vocab to piggy
+//  const words = ['美国','一代','美','代']; // from segmenter
+  const vocablist = vocab
+    .filter(((v)=>words.indexOf(v.s) > -1))
+    .map((v)=>`
+      <p class="vocab">
+        <span class="h">${v.s}</span><br/>
+        <span class="d">${v.d}</span>
+      </p>`)
+    .join('\n');
+
+  return vocablist;
 }
 
 
